c041892300cc7e4cd4d9cec76b220b3f54958f48
Uzair Iqbal <uzair.qq@outlook.com>
2018-09-07 05:28:19 +0500

    Created BundleConfig class and Method of Registers for css and javascript and created _ciew start and layout pages for not to callng css and javascript in all pages and for minification

--- TodoApp/TodoApp/Global.asax.cs
+++ TodoApp/TodoApp/Global.asax.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
+using TodoApp.App_Start;
 
 namespace TodoApp
 {
@@ -13,6 +15,7 @@ namespace TodoApp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
